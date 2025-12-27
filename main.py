25fd8bd92d3c20a80878aa3c1a98c8480463e5c7
sphuripat <72687181+sphuripat@users.noreply.github.com>
2025-12-27 07:38:59 +0700

    Remove old print statements and add new message

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
 ## first script
-print('hello world 2 from main.py')
-
-## print gooobye
-print('gooobye')
-
-##print new line
-print('new line')
-
-
-##def new function
-def add_two(num):
-  return num + 2
-
-
-print(add_two(2), add_two(5))
+print('github is easy')
